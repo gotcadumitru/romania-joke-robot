057d1ae08c72091405fd08f327da070dbd7790ca
gotcadumitru <64309696+gotcadumitru@users.noreply.github.com>
2020-09-30 12:55:39 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-// const button = document.getElementById('button');
-// const audioElement = document.getElementById('audio');
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
 
-// const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
-// async function getJoke(){
-//     const apiUrl = 'https://official-joke-api.appspot.com/jokes/programming/random';
-//     try{
-//         const respone = await fetch(apiUrl);
-//         const jokeJson = await respone.json();
-//         const joke = jokeJson[0].setup;
-//         VoiceRSS.speech({
-//             key: '8993dd44fef44b71acccc4aab2d87724',
-//             src: joke,
-//             hl: 'en-us',
-//             v: 'Linda',
-//             r: 0, 
-//             c: 'mp3',
-//             f: '44khz_16bit_stereo',
-//             ssml: false
-//         });
-//     }catch(error){
-//         console.log(error);
-//     }
-// }
-// button.addEventListener('click',getJoke);
+async function getJoke(){
+    const apiUrl = 'https://official-joke-api.appspot.com/jokes/programming/random';
+    try{
+        const respone = await fetch(apiUrl);
+        const jokeJson = await respone.json();
+        const joke = jokeJson[0].setup;
+        VoiceRSS.speech({
+            key: '8993dd44fef44b71acccc4aab2d87724',
+            src: joke,
+            hl: 'en-us',
+            v: 'Linda',
+            r: 0, 
+            c: 'mp3',
+            f: '44khz_16bit_stereo',
+            ssml: false
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
+button.addEventListener('click',getJoke);
 
 // const button = document.getElementById('button');
 // const audioElement = document.getElementById('audio');
@@ -36,18 +36,18 @@
 //     try{
 //         const respone = await fetch(proxyUrl + apiUrl);
 //         const jokeJson = await respone.json();
-//         // const joke = jokeJson.content;
+//         const joke = jokeJson.content;
 //        console.log(jokeJson);
-//         // VoiceRSS.speech({
-//         //     key: '8993dd44fef44b71acccc4aab2d87724',
-//         //     src: joke,
-//         //     hl: 'ru-ru',
-//         //     v: 'Linda',
-//         //     r: 0, 
-//         //     c: 'mp3',
-//         //     f: '44khz_16bit_stereo',
-//         //     ssml: false
-//         // });
+//         VoiceRSS.speech({
+//             key: '8993dd44fef44b71acccc4aab2d87724',
+//             src: joke,
+//             hl: 'ru-ru',
+//             v: 'Linda',
+//             r: 0, 
+//             c: 'mp3',
+//             f: '44khz_16bit_stereo',
+//             ssml: false
+//         });
 //     }catch(error){
 //         console.log(error);
 //     }
@@ -94,38 +94,38 @@
 
 
 
-const audioElement = document.getElementById('audio');
+// const audioElement = document.getElementById('audio');
 
-const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+// const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
-async function getJoke(){
-    const proxyUrl = 'https://quiet-wildwood-41923.herokuapp.com/';
-    const apiUrl = 'https://romanian-jokes-api.herokuapp.com/api/romanianjokes';
-   try{
-       console.log("ready");
-       $.get(apiUrl,
-        function(data){
-            console.log("ready");
-            try{ 
-                console.log("ready");
-            VoiceRSS.speech({
-                key: '8993dd44fef44b71acccc4aab2d87724',
-                src: data.joke,
-                hl: 'ro-ro',
-                v: 'Doru',
-                r: 0, 
-                c: 'mp3',
-                f: '16khz_8bit_mono',
-                ssml: false
-            });
-            }catch(error){
-                console.log(error);
-            }
-            console.log(data);
-        },'json');
-    }catch(error){
-    console.log(error);
-    }
-}
-$('#button').click(getJoke);
+// async function getJoke(){
+//     const proxyUrl = 'https://quiet-wildwood-41923.herokuapp.com/';
+//     const apiUrl = 'https://romanian-jokes-api.herokuapp.com/api/romanianjokes';
+//    try{
+//        console.log("ready");
+//        $.get(apiUrl,
+//         function(data){
+//             console.log("ready");
+//             try{ 
+//                 console.log("ready");
+//             VoiceRSS.speech({
+//                 key: '8993dd44fef44b71acccc4aab2d87724',
+//                 src: data.joke,
+//                 hl: 'ro-ro',
+//                 v: 'Doru',
+//                 r: 0, 
+//                 c: 'mp3',
+//                 f: '16khz_8bit_mono',
+//                 ssml: false
+//             });
+//             }catch(error){
+//                 console.log(error);
+//             }
+//             console.log(data);
+//         },'json');
+//     }catch(error){
+//     console.log(error);
+//     }
+// }
+// $('#button').click(getJoke);
 
